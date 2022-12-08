f1eb6e3e3ad5cdc1cc97aec58ede95611bab1893
Nguyễn Dương Quốc Thái <tesamwendai@hotmail.com>
2022-12-08 09:57:09 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
         /*
          * Application Service Providers...
          */
